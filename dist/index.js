7bf4c8f944d6182a68f77a5956eab5f2c26d56b0
valibali <pinkfloydhun@gmail.com>
2021-06-04 13:12:53 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -847,7 +847,7 @@ const userAgent = `octokit-endpoint.js/${VERSION} ${universalUserAgent.getUserAg
 
 const DEFAULTS = {
   method: "GET",
-  baseUrl: "https://api.github.com",
+  baseUrl: "https://github.boschdevcloud.com/api/v3",
   headers: {
     accept: "application/vnd.github.v3+json",
     "user-agent": userAgent
